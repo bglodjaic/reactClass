ff28990c8f75aae670f6ccbca3224f406495d8c9
Bojan Glodjaic <bojang@vast.com>
2017-07-14 00:08:45 +0200

    Initial commit

---
+++ client/index.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+
+class App extends React.Component {
+    render() {
+        return (
+            <div>Hello from React!!!!!</div>
+        );
+    }
+}
+
+ReactDOM.render(<App />, document.getElementById('root'));
